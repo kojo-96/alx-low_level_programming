9474a37fe7e9f0cdb1c5684221f33f0d59c5d86a
kojo-96 <evansnketsia@gmail.com>
2023-02-22 15:12:50 -0800

    stressinf me out

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,8 @@
 #include "main.h"
-<<<<<<< HEAD
-/**
- * main - print _putchar
- * is written by Evans Nketsia
-=======
 
 /**
  * main - print _putchar
- * is written by bwave
->>>>>>> f2010ded4fe5b29d67d4572f395d6f988f685a28
+ * is written by Evans Nketsia
  * Return: 0
  */
 
